5d792fd4b5f8fe0aea18ac85c088e6cdff4a42b5
MaxJayem <45623028+MaxJayem@users.noreply.github.com>
2018-12-30 12:13:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,16 @@
 		<script src="js/skel.min.js"></script>
 		<script src="js/skel-layers.min.js"></script>
 		<script src="js/init.js"></script>
+		
+		<script type="text/javascript" src="jquery.js"></script>    
+		<script type="text/javascript">
+			
+			function triggerSite() {
+				alert("Page is loaded");
+				$("#div1").load('trigger.php'); 
+			}
+		</script> 
+		
 		<noscript>
 			<link rel="stylesheet" href="css/skel.css" />
 			<link rel="stylesheet" href="css/style.css" />
@@ -23,7 +33,7 @@
 		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
 		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
 	</head>
-	<body class="landing">
+	<body class="landing" onload="triggerSite()">
 
 		<!-- Header -->
 			
@@ -55,81 +65,7 @@
 					<li><a href="szenario.html" class="button big">Zum Szenario</a></li>
 				</ul>
 			</section>
-<!-- Skript, dass die beiden anderen Seiten schon einmal lädt (Linda, Dialogflow-Webhook)-->
-	<?php
-
-							//Dialogflow-Fulfillment
-
-
- $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://ca1treament.herokuapp.com/");
-// Set so curl_exec returns the result instead of outputting it.
-
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-//BugfixPart1
-
-
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// Get the response and close the channel.
-$response = curl_exec($ch);
-
-if(curl_errno($ch)){
-    throw new Exception(curl_error($ch));
-}
-//BugfixPar2
-
-curl_close($ch);
-
-//-----------------------------------------------------------------------------------------------------------------------------
-
-								//Sarah1
- $ci = curl_init();
-curl_setopt($ci, CURLOPT_URL, "https://chatbotexperiment1.herokuapp.com/");
-
-curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, 10);
-curl_setopt($ci, CURLOPT_TIMEOUT, 30);
-//BugfixPart1
-
-// Set so curl_exec returns the result instead of outputting it.
-curl_setopt($ci, CURLOPT_RETURNTRANSFER, true);
-// Get the response and close the channel.
-$response1 = curl_exec($ci);
-
-if(curl_errno($ci)){
-    throw new Exception(curl_error($ci));
-}
-//BugfixPar2
-
-
-curl_close($ci);
-
-//-----------------------------------------------------------------------------------------------------------------------------
-
-									//Sarah2
-
- $cf = curl_init();
-curl_setopt($cf, CURLOPT_URL, "https://ca2experiment.herokuapp.com/");
-// Set so curl_exec returns the result instead of outputting it.
-
-curl_setopt($cf, CURLOPT_CONNECTTIMEOUT, 10);
-curl_setopt($cf, CURLOPT_TIMEOUT, 30);
-//BugfixPart1
-
-
-curl_setopt($cf, CURLOPT_RETURNTRANSFER, true);
-// Get the response and close the channel.
-$response2 = curl_exec($cf);
-
-if(curl_errno($cf)){
-    throw new Exception(curl_error($cf));
-}
-//BugfixPar2
-
-
-curl_close($cf);
-?>		
-
-
+<div id="div1"><h2>Test</h2></div>
+	
 	</body>
 </html>
