26fffd4921624221e063e431199eb6d53236d319
mjmgoethe <mjmgoethe@bitbucket.org>
2020-01-19 15:00:00 +0100

    asd

--- index.php
+++ index.php
@@ -15,16 +15,16 @@
 		<script src="js/skel.min.js"></script>
 		<script src="js/skel-layers.min.js"></script>
 		<script src="js/init.js"></script>
-		
-		<script type="text/javascript" src="jquery.js"></script>    
+
+		<script type="text/javascript" src="jquery.js"></script>
 		<script type="text/javascript">
-			
+
 			function triggerSite() {
-				
-				$("#div1").load('trigger.php'); 
+
+				$("#div1").load('trigger.php');
 			}
-		</script> 
-		
+		</script>
+
 		<noscript>
 			<link rel="stylesheet" href="css/skel.css" />
 			<link rel="stylesheet" href="css/style.css" />
@@ -36,30 +36,30 @@
 	<body class="landing" onload="triggerSite()">
 
 		<!-- Header -->
-			
 
 
 
-			
+
+
 		<!-- Three -->
 			<section id="three" class="wrapper style1">
 				<div class="container">
 					<header class="major">
 						<h2>Herzlich Wilkommen</h2>
-						<p>Das folgende Experiment besteht aus zwei Abschnitten: Einer kurzen Interaktion mit einem Chatbot, das heißt einer Software mit welcher Sie über natürliche Sprache kommunizieren können, und einer anschließenden Umfrage. 
-							Das Ziel der Interaktion mit dem Chatbot ist es, eine kleine Aufgabe im Kundenservice zu lösen, welche auf der folgenden Seite erklärt wird.
-							Im Anschluss erhalten Sie den Link zu der Umfrage.</br></br>
-							Die Dauer für die Teilnahme an dem Experiment beträgt <b>rund 10 Minuten</b>. 
-							Bitte klicken Sie nun auf den untenstehenden Link um das Experiment zu starten und lesen Sie sich die Hinweise aufmerksam durch. 
+						<p>Das folgende Experiment besteht aus zwei Abschnitten: Einer kurzen Interaktion mit einem Chatbot, das heißt einer Software mit welcher Sie über natürliche Sprache kommunizieren können, und einer anschließenden Umfrage.
+							Das Ziel der Interaktion mit dem Chatbot ist es, ein Rätsel zu lösen, indem dem Chatbot Ja-Nein-Fragen gestellt werden.
+							Nachdem Sie das Rätsel lösen konnten, erhalten sie den Link zu der Umfrage.</br></br>
+							Die Dauer für die Teilnahme an dem Experiment beträgt <b>rund 10 Minuten</b>.
+							Bitte klicken Sie nun auf den untenstehenden Link um das Experiment zu starten und lesen Sie sich die Hinweise aufmerksam durch.
 							Vielen Dank für Ihre Teilnahme.
-						
+
 						</p>
 					<div id="div1"><h2></h2></div>
 					</header>
 
 				</div>
 			</section>
-			
+
 		<!-- CTA -->
 			<section id="cta" class="wrapper style3">
 				<ul class="actions">
@@ -67,6 +67,6 @@
 				</ul>
 			</section>
 
-	
+
 	</body>
 </html>
